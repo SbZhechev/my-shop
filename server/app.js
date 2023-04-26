e83dd16e24b98a9cf87ef39aa10c7205a2c6672f
sbzhechev <sbzhechev@gmail.com>
2023-04-26 23:53:30 +0300

    Added endpoints for registering users and logging in.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,39 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const port = 3000;
+const connectionString = 'mongodb+srv://root:root@cluster0.c7fabeg.mongodb.net/my-shop';
+
+const UserModel = require('./models/UserModel');
 
 app.use(express.json());
+
+app.post('/sign-up', (req, res, next) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  if (!email) return res.status(400).send('Email is required!');
+  if (!password) return res.status(400).send('Password is required!');
+  
+  let user = new UserModel({ email, password });
+
+  user.save().then(() => {
+    res.status(200).send('User created!');
+  })
+});
+
+app.post('/login', async (req, res, next) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  if (!email) return res.status(400).send('Email is required!');
+  if (!password) return res.status(400).send('Password is required!');
+  
+  let user = await UserModel.findByEmail(email);
+  if (!user) return res.status(400).send('User does not exist!');
+  if (user.password !== password) return res.status(400).send('Wrong password!');
+
+  res.status(200).send('Login Successful!');
+});
 
 app.get('/', (req, res, next) => {
   res.send('Hello World');
@@ -10,4 +41,5 @@ app.get('/', (req, res, next) => {
 
 app.listen(port, () => {
   console.log(`App is listening on port: ${port}`);
+  mongoose.connect(connectionString).then(() => console.log('Connection Established'));
 });
