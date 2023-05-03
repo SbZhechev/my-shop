38c2a6f3698b5be2872ce0f4b07b96793927f0a2
sbzhechev <sbzhechev@gmail.com>
2023-05-03 16:38:25 +0300

    Added .env file with connection string

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config({ path: '../.env'});
 
 const app = express();
 const port = 3000;
-const connectionString = '';
 
 const UserModel = require('./models/UserModel');
 
@@ -41,5 +41,6 @@ app.get('/', (req, res, next) => {
 
 app.listen(port, () => {
   console.log(`App is listening on port: ${port}`);
+  const connectionString = process.env.DB_CONNECTION_STRING;
   mongoose.connect(connectionString).then(() => console.log('Connection Established'));
 });
