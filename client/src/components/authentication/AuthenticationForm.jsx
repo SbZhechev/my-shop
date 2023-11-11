1f2fac318174916daa36212ba356004cfeeac707
sbzhechev <sbzhechev@gmail.com>
2023-11-11 18:41:14 +0200

    Fixed attributes passed to button component.

--- client/src/components/authentication/AuthenticationForm.jsx
+++ client/src/components/authentication/AuthenticationForm.jsx
@@ -9,51 +9,49 @@ function AuthenticationForm({ legend, actionButtonText, actionButtonType }) {
   const feedbackMessage = useActionData();
 
   return (
-    <section>
-      <Form className='form' method='post' action="/signIn">
-        <fieldset className='form__fieldset'>
-          <legend className='form__legend'>{legend}</legend>
-          <label htmlFor='email' className='form__label'>
-            Email: 
-            <input 
-              id='email' 
-              type='email'
-              name='email'
-              placeholder='example@gmail.com' 
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-              className='form__input'
-            ></input>
-          </label>
-          <label htmlFor='password' className='form__label'>
-            Password:
-            <input 
-              id='password'
-              type='password'
-              name='password' 
-              value={password} 
-              onChange={(event => setPassword(event.target.value))}
-              className='form__input'
-            ></input>
-          </label>
-          <div className='form__actions'>
-            <Button active={true} type='submit' actionType={actionButtonType}>{actionButtonText}</Button>
-            { feedbackMessage ?
-              <p 
-                className={`
-                form__actions__feedbackMessage 
-                  ${feedbackMessage.isError ? "form__actions__feedbackMessage--error" : ''}
-                `}
-              >
-                {feedbackMessage.message}
-              </p>
-              :
-              null
-            }
-          </div>
-        </fieldset>
-      </Form>
-    </section>
+    <Form className='form' method='post' action="/signIn">
+      <fieldset className='form__fieldset'>
+        <legend className='form__legend'>{legend}</legend>
+        <label htmlFor='email' className='form__label'>
+          Email: 
+          <input 
+            id='email' 
+            type='email'
+            name='email'
+            placeholder='example@gmail.com' 
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            className='form__input'
+          ></input>
+        </label>
+        <label htmlFor='password' className='form__label'>
+          Password:
+          <input 
+            id='password'
+            type='password'
+            name='password' 
+            value={password} 
+            onChange={(event => setPassword(event.target.value))}
+            className='form__input'
+          ></input>
+        </label>
+        <div className='form__actions'>
+          <Button active={true} buttonType='submit' actionType={actionButtonType}>{actionButtonText}</Button>
+          { feedbackMessage ?
+            <p 
+              className={`
+              form__actions__feedbackMessage 
+                ${feedbackMessage.isError ? "form__actions__feedbackMessage--error" : ''}
+              `}
+            >
+              {feedbackMessage.message}
+            </p>
+            :
+            null
+          }
+        </div>
+      </fieldset>
+    </Form>
   );
 };
 
