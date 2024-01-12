872941b7e356cec570addc6a46e5cefc2d065808
sbzhechev <sbzhechev@gmail.com>
2024-01-12 22:01:54 +0200

    Moved authentication routes into separate file.

--- client/src/router/MainRouter.jsx
+++ client/src/router/MainRouter.jsx
@@ -1,11 +1,9 @@
-import { createBrowserRouter, redirect } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import App from '../components/App';
 import Home from '../components/home/Home';
-import Authentication from '../components/authentication/Authentication';
 import User from '../components/user/User';
 import { getUser } from '../services/AuthenticationService';
-import { handleLogin, handleSignUp } from '../services/AuthenticationService';
-import { AUTHENTICATION_FORM_TYPES } from '../utilites/constants';
+import { loginRoute, signUpRoute } from './routes/AuthenticationRoute';
 
 const router = createBrowserRouter([
   {
@@ -16,34 +14,8 @@ const router = createBrowserRouter([
         path: '/',
         element: <Home />
       },
-      {
-        path: '/auth?/login',
-        element: <Authentication authenticationFormType={AUTHENTICATION_FORM_TYPES.LOGIN_FORM} />,
-        action: async ({ request }) => {
-          try {
-            let data = await request.formData();
-
-            await handleLogin({ email: data.get('email'), password: data.get('password') });
-            return redirect('/user');
-          } catch (error) {
-            return { isError: true, message: error.response.data };
-          }
-        },
-      },
-      {
-        path: '/auth/signUp',
-        element: <Authentication authenticationFormType={AUTHENTICATION_FORM_TYPES.SIGN_UP_FORM} />,
-        action: async ({ request }) => {
-          try {
-            let data = await request.formData();
-  
-            await handleSignUp({ email: data.get('email'), password: data.get('password') });
-            return redirect('/auth/login');
-          } catch (error) {
-            return { isError: true, message: error.response.data };
-          }
-        }
-      },
+      loginRoute,
+      signUpRoute,
       {
         path: '/user',
         element: <User />
---
+++ client/src/router/routes/AuthenticationRoute.js
@@ -0,0 +1,36 @@
+import Authentication from "../../components/authentication/Authentication";
+import { handleLogin, handleSignUp } from '../../services/AuthenticationService';
+import { AUTHENTICATION_FORM_TYPES } from '../../utilites/constants';
+import { redirect } from "react-router-dom";
+
+const loginRoute = {
+  path: '/login',
+  element: <Authentication authenticationFormType={AUTHENTICATION_FORM_TYPES.LOGIN_FORM} />,
+  action: async ({ request }) => {
+    try {
+      let data = await request.formData();
+
+      await handleLogin({ email: data.get('email'), password: data.get('password') });
+      return redirect('/user');
+    } catch (error) {
+      return { isError: true, message: error.response.data };
+    }
+  }
+};
+
+const signUpRoute = {
+  path: '/signUp',
+  element: <Authentication authenticationFormType={AUTHENTICATION_FORM_TYPES.SIGN_UP_FORM} />,
+  action: async ({ request }) => {
+    try {
+      let data = await request.formData();
+
+      await handleSignUp({ email: data.get('email'), password: data.get('password') });
+      return redirect('/auth/login');
+    } catch (error) {
+      return { isError: true, message: error.response.data };
+    }
+  }
+};
+
+export { loginRoute, signUpRoute };
